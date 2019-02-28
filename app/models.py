a3491565c6a8b46d0192368fa18ba872d6f8dc17
jym503558564 <503558564@qq.com>
2019-02-28 21:51:55 +0800

    [Create]创建文章表

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from config import db, login_manager
 from flask_login import UserMixin
 
 
-#创建表
+#创建用户表
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -19,6 +19,25 @@ class User(db.Model, UserMixin):
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+
+#创建文章表
+class Article(db.Model):
+    __tablename__ = 'articles'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(256))
+    content = db.Column(db.Text)# 关联表，这里要与相关联的表的类型一直, user.id 表示关联到user表下的id字段
+
+    # author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    # 给这个article模型添加一个author属性（关系表），User为要连接的表，backref为定义反向引用
+    # author = db.relationship('User', backref=db.backref('articles'), lazy='dynamic')
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
